d7e39d0b76723f23be95ed75f88d390756df68f0
mark.collier <mark.collier@qtsus-MacBook-Pro.local>
2019-03-05 17:28:05 +0800

    Lifecycle Hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LoggerService }    from './logger.service';
+
+let order = 1;
+let happens = 0;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'peek';
+export class AppComponent implements
+ OnInit,OnChanges /*, DoCheck,
+AfterContentInit, AfterContentChecked,
+AfterViewInit, AfterViewChecked, OnDestroy*/ {
+
+  ngOnInit(){
+    this.logIt("OnInit");
+  }
+  ngOnChanges(){
+    this.logIt("OnChanges");
+  }
+  constructor(private logger:LoggerService){}
+  logIt(msg: string) {
+    if(msg=="clear")return this.logger.clear();
+    order += this.logger.log(`#${order} ${msg}`);
+  }
+
 }
---
+++ src/app/logger.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+providedIn:"root"
+})
+export class LoggerService {
+  logs: string[] = [];
+  prevMsg = '';
+  prevMsgCount = 1;
+
+  log(msg: string)  {
+    if (msg === this.prevMsg) {
+      // Repeat message; update last log entry with count.
+      this.logs[this.logs.length - 1] = msg + ` (${this.prevMsgCount += 1}x)`;
+      return 0;
+    } else {
+      // New message; log it.
+      this.prevMsg = msg;
+      this.prevMsgCount = 1;
+      this.logs.push(msg);
+      return 1;
+    }
+  }
+
+  clear() { this.logs = []; return 0;}
+
+  // schedules a view refresh to ensure display catches up
+  tick() {  this.tick_then(() => { }); }
+  tick_then(fn: () => any) { setTimeout(fn, 0); }
+}
