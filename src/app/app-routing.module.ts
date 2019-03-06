96f2c13a77b0efe423c4721c45395f90e7ff2839
mark.collier <mark.collier@qtsus-MacBook-Pro.local>
2019-03-06 17:06:18 +0800

    added views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SpyParentComponent } from './spy/spy.component'
+
+import {
+  AfterContentParentComponent,
+  AfterContentComponent,
+  ChildComponent
+} from './hooks/after-content.component';
+
+import {
+  AfterViewParentComponent,
+  AfterViewComponent,
+  ChildViewComponent
+} from './hooks/after-view.component';
+
+import {
+  CounterParentComponent,
+  MyCounterComponent
+} from './hooks/counter.component';
+
+import {
+  DoCheckParentComponent,
+  DoCheckComponent
+} from './hooks/do-check.component';
+
+import {
+  OnChangesParentComponent,
+  OnChangesComponent
+} from './hooks/on-changes.component';
+
+import { SpyParentComponent } from './spy/spy.component';
+
+
 const routes: Routes = [
-  {path: 'spy', component:SpyParentComponent}
+  {path: 'OnChanges', component:OnChangesParentComponent},
+  {path: 'DoCheck',component:DoCheckParentComponent},
+  {path: 'DoCheck/child',component:DoCheckComponent},
+  {path: 'AfterContent',component:AfterContentParentComponent},
+  {path: 'AfterView', component:AfterViewParentComponent},
+  {path: 'AfterView/child',component:AfterViewComponent},
+  {path: 'AfterView/childView',component:ChildViewComponent},
+  {path: 'spy', component:SpyParentComponent},
+  {path: 'counter', component:CounterParentComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,60 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {
+  AfterContentParentComponent,
+  AfterContentComponent,
+  ChildComponent
+} from './hooks/after-content.component';
+
+import {
+  AfterViewParentComponent,
+  AfterViewComponent,
+  ChildViewComponent
+} from './hooks/after-view.component';
+
+import {
+  CounterParentComponent,
+  MyCounterComponent
+} from './hooks/counter.component';
+
+import {
+  DoCheckParentComponent,
+  DoCheckComponent
+} from './hooks/do-check.component';
+
+import {
+  OnChangesParentComponent,
+  OnChangesComponent
+} from './hooks/on-changes.component';
+
 import { SpyParentComponent } from './spy/spy.component';
-import { SpyDirective } from './spy/spy.directive'
+import { SpyDirective } from './spy/spy.directive';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    SpyParentComponent,
-    SpyDirective
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    AfterContentParentComponent,
+    AfterContentComponent,
+    ChildComponent,
+    AfterViewParentComponent,
+    AfterViewComponent,
+    ChildViewComponent,
+    CounterParentComponent,
+    MyCounterComponent,
+    DoCheckParentComponent,
+    DoCheckComponent,
+    OnChangesParentComponent,
+    OnChangesComponent,
+    SpyParentComponent,
+    SpyDirective
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
